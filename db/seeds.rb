3bffb81ce47df56583c9c9370c8528de77ddbd29
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-24 16:16:04 -0500

    Config index action and seeds data for greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "-Hello there... // -General Kenobi")
+Greeting.create(message: "-Hi yourself")
+Greeting.create(message: "-Greetings Traveler")
+Greeting.create(message: "-We're all pretty bizarre. Some of us are just better at hiding it, that's all.")
+Greeting.create(message: "-Hakuna Matata… it means no worries!")
+Greeting.create(message: "-Constantly talking isn't necessarily communicating.")
+Greeting.create(message: "-Stop right there, criminal scum!")
+Greeting.create(message: "-You had me at hello")
+Greeting.create(message: "-Its over Anakin, I got the higher ground!")
+Greeting.create(message: "-Going in one more round when you don't think you can. That's what makes all the difference in your life.")
